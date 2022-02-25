332425459ab12c083cb91a2069762416768a05e4
Suzy Q <blakmagjick@users.noreply.github.com>
2022-02-24 21:18:39 -0800

    Added cors whitelist for frontend requests

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 const express = require('express')
 const routes = require('./routes')
+const cors = require('cors')
 const PORT = 3003
 const app = express()
 
 require('./config/db.connection')
 
 app.use(express.json())
+
+const whitelist = ['http://localhost:3000']
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (whitelist.indexOf(origin) !== -1 || !origin) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    }
+}
+app.use(cors(corsOptions))
+
 
 app.get('/', (req, res) => {
     res.send('Route connected')
